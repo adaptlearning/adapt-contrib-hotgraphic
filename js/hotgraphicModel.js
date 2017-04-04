d645b302196e1dea6bc386c7a5605e5399e37f95
Thomas Berger <thomas.berger@learnchamp.com>
2017-04-04 14:02:56 +0200

    use model and View

---
+++ js/hotgraphicModel.js
@@ -0,0 +1,12 @@
+define([
+    'core/js/adapt',
+    'core/js/models/itemsModel'
+], function(Adapt, ItemsModel) {
+
+    var HotgraphicModel = ItemsModel.extend({
+        
+    });
+
+    return HotgraphicModel;
+
+});
