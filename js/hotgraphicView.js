70354ac4c4154ee0ba7c8149970627da56f827f7
Guy Willis <guy.willis@kineo.com>
2019-01-03 11:56:11 +0000

    Converted tabs to spaces, indentation amends

--- js/hotgraphicView.js
+++ js/hotgraphicView.js
@@ -1,176 +1,176 @@
 define([
-    'core/js/adapt',
-    'core/js/views/componentView',
-    './hotgraphicPopupView'
+  'core/js/adapt',
+  'core/js/views/componentView',
+  './hotgraphicPopupView'
 ], function(Adapt, ComponentView, HotgraphicPopupView) {
-    'use strict';
+  'use strict';
 
-    var HotGraphicView = ComponentView.extend({
+  var HotGraphicView = ComponentView.extend({
 
-        events: {
-            'click .hotgraphic-graphic-pin': 'onPinClicked'
-        },
+    events: {
+      'click .hotgraphic-graphic-pin': 'onPinClicked'
+    },
 
-        initialize: function() {
-            ComponentView.prototype.initialize.call(this);
-            this.setUpViewData();
-            this.setUpModelData();
-            this.setUpEventListeners();
-            this.checkIfResetOnRevisit();
-        },
+    initialize: function() {
+      ComponentView.prototype.initialize.call(this);
+      this.setUpViewData();
+      this.setUpModelData();
+      this.setUpEventListeners();
+      this.checkIfResetOnRevisit();
+    },
 
-        setUpViewData: function() {
-            this.popupView = null;
-            this._isPopupOpen = false;
-        },
+    setUpViewData: function() {
+      this.popupView = null;
+      this._isPopupOpen = false;
+    },
 
-        setUpModelData: function() {
-            if (this.model.get('_canCycleThroughPagination') === undefined) {
-                this.model.set('_canCycleThroughPagination', false);
-            }
-        },
+    setUpModelData: function() {
+      if (this.model.get('_canCycleThroughPagination') === undefined) {
+        this.model.set('_canCycleThroughPagination', false);
+      }
+    },
 
-        setUpEventListeners: function() {
-            this.listenTo(Adapt, 'device:changed', this.reRender);
+    setUpEventListeners: function() {
+      this.listenTo(Adapt, 'device:changed', this.reRender);
 
-            this.listenTo(this.model.get('_children'), {
-                'change:_isActive': this.onItemsActiveChange,
-                'change:_isVisited': this.onItemsVisitedChange
-            });
-        },
+      this.listenTo(this.model.get('_children'), {
+        'change:_isActive': this.onItemsActiveChange,
+        'change:_isVisited': this.onItemsVisitedChange
+      });
+    },
 
-        reRender: function() {
-            if (Adapt.device.screenSize !== 'large') {
-                this.replaceWithNarrative();
-            }
-        },
+    reRender: function() {
+      if (Adapt.device.screenSize !== 'large') {
+        this.replaceWithNarrative();
+      }
+    },
 
-        replaceWithNarrative: function() {
-            var NarrativeView = Adapt.getViewClass('narrative');
+    replaceWithNarrative: function() {
+      var NarrativeView = Adapt.getViewClass('narrative');
 
-            var model = this.prepareNarrativeModel();
-            var newNarrative = new NarrativeView({ model: model });
-            var $container = $(".component-container", $("." + this.model.get("_parentId")));
+      var model = this.prepareNarrativeModel();
+      var newNarrative = new NarrativeView({ model: model });
+      var $container = $(".component-container", $("." + this.model.get("_parentId")));
 
-            newNarrative.reRender();
-            newNarrative.setupNarrative();
-            $container.append(newNarrative.$el);
-            Adapt.trigger('device:resize');
-            _.defer(this.remove.bind(this));
-        },
+      newNarrative.reRender();
+      newNarrative.setupNarrative();
+      $container.append(newNarrative.$el);
+      Adapt.trigger('device:resize');
+      _.defer(this.remove.bind(this));
+    },
 
-        prepareNarrativeModel: function() {
-            var model = this.model;
-            model.set({
-                '_component': 'narrative',
-                '_wasHotgraphic': true,
-                'originalBody': model.get('body'),
-                'originalInstruction': model.get('instruction')
-            });
+    prepareNarrativeModel: function() {
+      var model = this.model;
+      model.set({
+        '_component': 'narrative',
+        '_wasHotgraphic': true,
+        'originalBody': model.get('body'),
+        'originalInstruction': model.get('instruction')
+      });
 
-            // Check if active item exists, default to 0
-            var activeItem = model.getActiveItem();
-            if (!activeItem) {
-                model.getItem(0).toggleActive(true);
-            }
+      // Check if active item exists, default to 0
+      var activeItem = model.getActiveItem();
+      if (!activeItem) {
+        model.getItem(0).toggleActive(true);
+      }
 
-            // Swap mobile body and instructions for desktop variants.
-            if (model.get('mobileBody')) {
-                model.set('body', model.get('mobileBody'));
-            }
-            if (model.get('mobileInstruction')) {
-                model.set('instruction', model.get('mobileInstruction'));
-            }
+      // Swap mobile body and instructions for desktop variants.
+      if (model.get('mobileBody')) {
+        model.set('body', model.get('mobileBody'));
+      }
+      if (model.get('mobileInstruction')) {
+        model.set('instruction', model.get('mobileInstruction'));
+      }
 
-            return model;
-        },
+      return model;
+    },
 
-        onItemsActiveChange: function(model, _isActive) {
-            this.getItemElement(model).toggleClass('active', _isActive);
-        },
+    onItemsActiveChange: function(model, _isActive) {
+      this.getItemElement(model).toggleClass('active', _isActive);
+    },
 
-        getItemElement: function(model) {
-            var index = model.get('_index');
-            return this.$('.hotgraphic-graphic-pin').filter('[data-index="' + index + '"]');
-        },
+    getItemElement: function(model) {
+      var index = model.get('_index');
+      return this.$('.hotgraphic-graphic-pin').filter('[data-index="' + index + '"]');
+    },
 
-        onItemsVisitedChange: function(model, _isVisited) {
-            if (!_isVisited) return;
-            var $pin = this.getItemElement(model);
+    onItemsVisitedChange: function(model, _isVisited) {
+      if (!_isVisited) return;
+      var $pin = this.getItemElement(model);
 
-            // Append the word 'visited.' to the pin's aria-label
-            var visitedLabel = this.model.get('_globals')._accessibility._ariaLabels.visited + ".";
-            $pin.attr('aria-label', function(index, val) {
-                return val + " " + visitedLabel;
-            });
+      // Append the word 'visited.' to the pin's aria-label
+      var visitedLabel = this.model.get('_globals')._accessibility._ariaLabels.visited + ".";
+      $pin.attr('aria-label', function(index, val) {
+        return val + " " + visitedLabel;
+      });
 
-            $pin.addClass('visited');
-        },
+      $pin.addClass('visited');
+    },
 
-        checkIfResetOnRevisit: function() {
-            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+    checkIfResetOnRevisit: function() {
+      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
-            // If reset is enabled set defaults
-            if (isResetOnRevisit) {
-                this.model.reset(isResetOnRevisit);
-            }
-        },
+      // If reset is enabled set defaults
+      if (isResetOnRevisit) {
+        this.model.reset(isResetOnRevisit);
+      }
+    },
 
-        preRender: function() {
-            if (Adapt.device.screenSize === 'large') {
-                this.render();
-            } else {
-                this.reRender();
-            }
-        },
+    preRender: function() {
+      if (Adapt.device.screenSize === 'large') {
+        this.render();
+      } else {
+        this.reRender();
+      }
+    },
 
-        postRender: function() {
-            this.renderState();
-            this.$('.hotgraphic-widget').imageready(this.setReadyStatus.bind(this));
-            if (this.model.get('_setCompletionOn') === 'inview') {
-                this.setupInviewCompletion('.component-widget');
-            }
-        },
+    postRender: function() {
+      this.renderState();
+      this.$('.hotgraphic-widget').imageready(this.setReadyStatus.bind(this));
+      if (this.model.get('_setCompletionOn') === 'inview') {
+        this.setupInviewCompletion('.component-widget');
+      }
+    },
 
-        onPinClicked: function (event) {
-            if(event) event.preventDefault();
+    onPinClicked: function (event) {
+      if (event) event.preventDefault();
 
-            var item = this.model.getItem($(event.currentTarget).data('index'));
-            item.toggleActive(true);
-            item.toggleVisited(true);
+      var item = this.model.getItem($(event.currentTarget).data('index'));
+      item.toggleActive(true);
+      item.toggleVisited(true);
 
-            this.openPopup();
-        },
+      this.openPopup();
+    },
 
-        openPopup: function() {
-            if (this._isPopupOpen) return;
+    openPopup: function() {
+      if (this._isPopupOpen) return;
 
-            this._isPopupOpen = true;
+      this._isPopupOpen = true;
 
-            this.popupView = new HotgraphicPopupView({
-                model: this.model
-            });
+      this.popupView = new HotgraphicPopupView({
+        model: this.model
+      });
 
-            Adapt.trigger("notify:popup", {
-                _view: this.popupView,
-                _isCancellable: true,
-                _showCloseButton: false,
-                _closeOnBackdrop: true,
-                _classes: ' hotgraphic'
-            });
+      Adapt.trigger("notify:popup", {
+        _view: this.popupView,
+        _isCancellable: true,
+        _showCloseButton: false,
+        _closeOnBackdrop: true,
+        _classes: ' hotgraphic'
+      });
 
-            this.listenToOnce(Adapt, {
-                'popup:closed': this.onPopupClosed
-            });
-        },
+      this.listenToOnce(Adapt, {
+        'popup:closed': this.onPopupClosed
+      });
+    },
 
-        onPopupClosed: function() {
-            this.model.getActiveItem().toggleActive();
-            this._isPopupOpen = false;
-        }
+    onPopupClosed: function() {
+      this.model.getActiveItem().toggleActive();
+      this._isPopupOpen = false;
+    }
 
-    });
+  });
 
-    return HotGraphicView;
+  return HotGraphicView;
 
 });
