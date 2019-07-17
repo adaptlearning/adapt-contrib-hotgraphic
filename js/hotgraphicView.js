514ee8f0ba8a2d28e7cab65f832914aee18f2ef0
Oliver Foster <oliver.foster@kineo.com>
2019-07-17 09:23:45 +0100

    issue/2451 recommendation
    Co-Authored-By: Matt Leathes <mleathes+github@gmail.com>

--- js/hotgraphicView.js
+++ js/hotgraphicView.js
@@ -101,7 +101,7 @@ define([
             // Append the word 'visited.' to the pin's aria-label
             var visitedLabel = this.model.get('_globals')._accessibility._ariaLabels.visited + ".";
             $pin.find('.aria-label').each(function(index, el) {
-                el.innerHTML = el.innerHTML + " " + visitedLabel;
+                el.innerHTML += " " + visitedLabel;
             });
 
             $pin.addClass('visited');
