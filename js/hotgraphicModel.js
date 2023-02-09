eaabd42c9747d62207b4135f94c9a72439c0e522
Brad Simpson <brad.simpson@kineo.com>
2023-02-09 14:40:39 -0700

    #248 Pin default value

--- js/hotgraphicModel.js
+++ js/hotgraphicModel.js
@@ -4,12 +4,10 @@ export default class HotgraphicModel extends ItemsComponentModel {
 
   setUpItems() {
     super.setUpItems();
-    this.getChildren().forEach((child, index) => {
+
+    this.getChildren().forEach((child) => {
       // Set _pin for the item if undefined
-      const pin = child.get('_pin');
-      if (!pin) {
-        child.set('_pin', false);
-      }
+      if (!child.get('_pin')) child.set('_pin', false);
     });
   }
 
