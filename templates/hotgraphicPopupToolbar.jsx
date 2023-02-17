bc3d89ab1159740325dc29cfb71d3fc2edd00752
Brad Simpson <brad.simpson@kineo.com>
2023-02-17 10:08:00 -0700

    #248 Hide popup count from screenreaders

--- templates/hotgraphicPopupToolbar.jsx
+++ templates/hotgraphicPopupToolbar.jsx
@@ -47,6 +47,7 @@ export default function HotgraphicPopupToolbar(props) {
         <div
           className="hotgraphic-popup__count"
           dangerouslySetInnerHTML={{ __html: itemCount }}
+          aria-hidden="true"
         />
 
         <button
