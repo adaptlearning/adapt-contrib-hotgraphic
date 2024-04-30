d7b376a31c141ad91eea0ea324fe3030c6ba40ea
Oliver Foster <oliver.foster@kineo.com>
2024-04-30 20:55:57 +0100

    New: Added static tooltips

--- js/hotgraphicModel.js
+++ js/hotgraphicModel.js
@@ -12,6 +12,7 @@ export default class HotgraphicModel extends ItemsComponentModel {
   setUpItems() {
     super.setUpItems();
     const id = this.get('_id');
+    const hasStaticTooltips = this.get('_hasStaticTooltips') ?? false;
     this.getChildren().forEach((child, index) => {
 
       // Set _pin for the item if undefined
@@ -19,14 +20,23 @@ export default class HotgraphicModel extends ItemsComponentModel {
 
       const tooltip = child.get('_tooltip');
       if (!tooltip?._isEnabled) return;
+
       tooltip._id = `hotgraphic-pin-${id}-${index}`;
       const tooltipConfig = {
+        _isStatic: hasStaticTooltips,
         ...child.toJSON(),
         _classes: [ 'hotgraphic__pin-tooltip' ],
         ...tooltip
       };
       tooltipConfig._position = tooltipConfig._position || 'outside bottom middle middle';
-      tooltips.register(tooltipConfig);
+      const tooltipModel = tooltips.register(tooltipConfig);
+      child.on('change', () => {
+        tooltipModel.set({
+          ...child.toJSON(),
+          _classes: [ 'hotgraphic__pin-tooltip' ],
+          ...tooltip
+        });
+      });
 
     });
   }
