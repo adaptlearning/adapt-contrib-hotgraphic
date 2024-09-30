bce4f45fb3def6cb91c4d66d3a7dd6c896698455
Brad Simpson <brad.simpson@kineo.com>
2024-09-30 08:33:40 -0600

    Move numbered pin class to pin buttons

--- templates/hotgraphic.jsx
+++ templates/hotgraphic.jsx
@@ -5,7 +5,6 @@ export default function Hotgraphic(props) {
 
   const {
     _graphic,
-    _useNumberedPins,
     _useGraphicsAsPins
   } = props;
 
@@ -17,7 +16,6 @@ export default function Hotgraphic(props) {
       <div className={classes([
         'component__widget hotgraphic__widget',
         _useGraphicsAsPins ? 'is-tile' : 'is-pin',
-        _useNumberedPins && 'has-numbered-pins',
         _graphic.attribution && 'has-attribution'
       ])}>
 
