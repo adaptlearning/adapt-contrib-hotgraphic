af89644150e962f5993ec8ed3ba1a5bb59c638be
Brad Simpson <brad.simpson@kineo.com>
2024-09-30 08:34:09 -0600

    Fix: Allow additional custom classes on tooltips (fixes #316) (#317)

--- js/hotgraphicModel.js
+++ js/hotgraphicModel.js
@@ -21,10 +21,13 @@ export default class HotgraphicModel extends ItemsComponentModel {
       const tooltip = child.get('_tooltip');
       if (!tooltip?._isEnabled) return;
       tooltip._id = `hotgraphic-pin-${id}-${index}`;
+      tooltip._classes = [
+        tooltip._classes,
+        'hotgraphic__pin-tooltip'
+      ].filter(Boolean).join(' ');
       const tooltipConfig = {
         _isStatic: hasStaticTooltips,
         ...child.toJSON(),
-        _classes: [ 'hotgraphic__pin-tooltip' ],
         ...tooltip
       };
       tooltipConfig._position = tooltipConfig._position || 'outside bottom middle middle';
@@ -32,7 +35,6 @@ export default class HotgraphicModel extends ItemsComponentModel {
       child.on('change', () => {
         tooltipModel.set({
           ...child.toJSON(),
-          _classes: [ 'hotgraphic__pin-tooltip' ],
           ...tooltip
         });
       });
