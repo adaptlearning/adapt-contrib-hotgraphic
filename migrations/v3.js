768016870a09d9a53f1aba6a00a580e058f23ea7
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-03-28 13:31:36 +0000

    typo

--- migrations/v3.js
+++ migrations/v3.js
@@ -8,7 +8,7 @@ describe('Hot Graphic - v2.1.2 to v3.0.0', async () => {
   whereFromPlugin('Hot Graphic - from v2.1.2', { name: 'adapt-contrib-hotgraphic', version: '<3.0.0' });
 
   whereContent('Hot Graphic - where hotgraphic', async content => {
-    hotgraphics = getComponents(content, 'hotgraphic');
+    hotgraphics = getComponents('hotgraphic');
     return hotgraphics.length;
   });
 
